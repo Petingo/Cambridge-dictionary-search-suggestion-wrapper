ad2de12ef1be7d555e4977b4a989486d677d6b6e
Petingo <petingo@ip137-58.wlan.ntust.edu.tw>
2019-05-28 13:55:06 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cambridge dictionary search suggestion wrapper
+Generalize the search suggestion of Cambridge dictionary for Vivaldi custom search engine.
---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const https = require('https')
+const url = require('url')
+
+const server = http.createServer(function (req, res) {
+    let pathName = url.parse(req.url, true, false).pathname
+    let queryWord = url.parse(req.url).query
+
+    if (pathName == "/") {
+        let queryUrl = `https://dictionary.cambridge.org/zht/autocomplete/english-chinese-traditional/?q=${queryWord}`;
+        https.get(queryUrl, (queryResponse) => {
+            
+            let q = ""
+            queryResponse.on("data", (data) => {
+                q += data
+            })
+
+            queryResponse.on("end", () => {
+                q = JSON.parse(q);
+                let vocList = []
+                for(element of q.results){
+                    vocList.push(element.searchtext)
+                }
+                let result = JSON.stringify([queryWord, vocList])
+
+                res.writeHead(200, { "Content-Type": "application/json" })
+                res.end(result);
+            })
+        })
+
+    }
+
+})
+
+server.listen(8888);
+console.log("Server is listening!");
